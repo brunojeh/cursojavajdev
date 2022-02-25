9bbabbf3054754723c7620d3aa50613d7671e964
brunojeh <bruno_areda@hotmail.com>
2022-02-25 08:30:03 -0300

    Commit curso Thread

--- cursojavajdev/src/cursojava/thread/ExecutarTela.java
+++ cursojavajdev/src/cursojava/thread/ExecutarTela.java
@@ -3,7 +3,8 @@ package cursojava.thread;
 public class ExecutarTela {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		TelaTimeThread telaTimeThread = new TelaTimeThread();
 
 	}
 
